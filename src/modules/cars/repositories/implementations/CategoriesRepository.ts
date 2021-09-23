57ecafed922adfb55bcc98eabf641f0094a4b7d8
Lucas Kaíque <lucas.kaique175@hotmail.com>
2021-09-23 17:38:58 -0300

    Finished categories and specifications methods

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,16 +1,25 @@
 import { v4 as uuidV4 } from "uuid";
 
-import { Category } from "../model/Category";
+import { Category } from "../../model/Category";
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from "./ICategoriesRepository";
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,51 @@
+import { v4 as uuidV4 } from "uuid";
+
+import { Specification } from "../../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from "../ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
+    this.specifications = [];
+  }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+    return SpecificationsRepository.INSTANCE;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      id: uuidV4(),
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+
+  list(): Specification[] {
+    return this.specifications;
+  }
+}
+
+export { SpecificationsRepository };
