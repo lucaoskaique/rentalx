4c6eef4a83aeb44c944450404c7de52e82948d29
lucaoskaique <lucas.kaique175@hotmail.com>
2022-02-18 19:20:34 -0300

    🚀 created Authentication UseCase and Controller

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,51 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: { email: string; name: string };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("E-mail or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("E-mail or password incorrect!");
+    }
+
+    const token = sign({}, "34e286847ce8b31bd0e51b400a12ef19", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      user: { name: user.name, email: user.email },
+      token,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
