81e1bb523d2475f0266f15e4c45277afdafaf456
lucaoskaique <lucas.kaique175@hotmail.com>
2022-02-18 15:10:43 -0300

    🚀 created migrations: delete username from USERS

---
+++ src/database/migrations/1645207814934-AlterUsersDeleteUsername.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterUsersDeleteUsername1645207814934 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
