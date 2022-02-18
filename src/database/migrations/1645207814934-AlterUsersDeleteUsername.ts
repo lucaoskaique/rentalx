e05f32dfbe7eb5738e4b1458afb7e560ee8b6881
lucaoskaique <lucas.kaique175@hotmail.com>
2022-02-18 15:14:43 -0300

    🚀 delete username from USERS

--- src/database/migrations/1645207814934-AlterUsersDeleteUsername.ts
+++ src/database/migrations/1645207814934-AlterUsersDeleteUsername.ts
@@ -1,11 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
-export class AlterUsersDeleteUsername1645207814934 implements MigrationInterface {
+export class AlterUsersDeleteUsername1645207814934
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "username");
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "username",
+        type: "varchar",
+      })
+    );
+  }
 }
