27c2ecefe84f82c4c8d1a7710c62a1338613db90
lucaoskaique <lucas.kaique175@hotmail.com>
2022-02-18 15:04:42 -0300

    🚀 created User UseCase

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    password,
+    username,
+    email,
+    driver_licence,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByName(name);
+
+    if (userAlreadyExists) {
+      throw new Error("User Already Exists!");
+    }
+
+    this.usersRepository.create({
+      name,
+      password,
+      username,
+      email,
+      driver_licence,
+    });
+  }
+}
+
+export { CreateUserUseCase };
