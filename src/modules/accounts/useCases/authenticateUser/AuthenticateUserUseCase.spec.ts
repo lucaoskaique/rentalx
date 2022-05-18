57b7f7e11b7484328abe8183bf01a08bedc17b84
lucaoskaique <lucas.kaique175@hotmail.com>
2022-05-18 20:00:29 -0300

    finished spec authenticate

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,49 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/inMemory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUserCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUserCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("Shoud be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      driver_licence: "123456789",
+      email: "lucas@hotmail.com",
+      name: "User Teste",
+      password: "123456",
+    };
+    await createUserUseCase.execute(user);
+    const userAuthenticated = await authenticateUserUserCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(userAuthenticated).toHaveProperty("token");
+  });
+
+  it("Shoud be able to authenticate an user", async () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_licence: "123456789",
+        email: "lucas@hotmail.com",
+        name: "User Teste",
+        password: "123456",
+      };
+      await authenticateUserUserCase.execute({
+        email: user.email,
+        password: user.password,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
