9f3fd866c9b3b59c5ee35a5cd5467ef0dd55c5a9
lucaoskaique <lucas.kaique175@hotmail.com>
2022-11-03 21:30:19 -0300

    changed field inside cars migration

--- src/shared/infra/database/migrations/1653311875476-CreateCars.ts
+++ src/shared/infra/database/migrations/1653311875476-CreateCars.ts
@@ -29,7 +29,7 @@ export class CreateCars1653311875476 implements MigrationInterface {
             default: true,
           },
           {
-            name: "licence_plate",
+            name: "license_plate",
             type: "varchar",
           },
           {
