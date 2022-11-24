e50eb0b847fd1b33f1019cbd62c06f3825042ea9
lucaoskaique <lucas.kaique175@hotmail.com>
2022-11-24 09:50:39 -0300

    finished create rentals

--- src/modules/rentals/useCases/createRental/createRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/createRentalUseCase.ts
@@ -1,18 +1,63 @@
-import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+dayjs.extend(utc);
 interface IRequest {
   user_id: string;
   car_id: string;
   expected_return_date: Date;
 }
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) {}
 
   async execute({
     car_id,
     expected_return_date,
     user_id,
-  }: IRequest): Promise<void> {}
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError("There's a rental in progress for user!");
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compare < 24) {
+      throw new AppError("Invalid return time!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      car_id,
+      expected_return_date,
+      user_id,
+    });
+
+    return rental;
+  }
 }
 
 export { CreateRentalUseCase };
