27f945ab4e705e33d662efbb6b92415737f5e228
lucaoskaique <lucas.kaique175@hotmail.com>
2022-11-25 22:00:12 -0300

    finished all modules

--- src/shared/infra/database/index.ts
+++ src/shared/infra/database/index.ts
@@ -1,22 +1,26 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
+// interface IOptions {
+//   host: string;
+// }
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
-    ...options,
-  });
-});
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   createConnection({
+//     ...options,
+//   });
+// });
 
 export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      host, // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      host: process.env.NODE_ENV === "test" ? "localhost" : host, // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentx_test"
+          : defaultOptions.database,
     })
   );
 };
